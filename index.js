33490ec00145a564379e52ccb89aff82e95ecece
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-17 03:15:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,12 @@
 
 const Driver = {};
 
+function 
+updateDriverWithKeyAndValue(Driver, key, value){
+  const newDriver = { ...obj };
+
+  newDriver[key] = value;
+
+  return newDriver;
+}
 const Driver = updateDriverWithKeyAndValue(newDriver, key, value){};
